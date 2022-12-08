6126d17172e485353fa224d712f33ad95d747efa
Matt Tennison <matt@tennison.dev>
2022-12-08 00:13:32 +0000

    Write minimalism:photo:list item to KV in cron job

--- src/load-data.ts
+++ src/load-data.ts
@@ -3,9 +3,26 @@ import { search } from './services/photo'
 export async function handleScheduled() {
   const results = await search('minimalism')
 
-  await Promise.all(
-    results.photos.map((photo, index) =>
-      PHOTOS.put(`minimalism:photo:${index}`, JSON.stringify(photo)),
+  const individualPhotoItems = results.photos.map((photo) => ({
+    key: `minimalism:photo:${photo.id}`,
+    value: photo,
+  }))
+
+  const itemsToWrite = [
+    ...individualPhotoItems,
+    {
+      key: 'minimalism:photo:list',
+      value: {
+        keys: results.photos.map((photo) => ({
+          name: `minimalism:photo:${photo.id}`,
+        })),
+      },
+    },
+  ]
+
+  await Promise.all([
+    ...itemsToWrite.map(({ key, value }) =>
+      PHOTOS.put(key, JSON.stringify(value)),
     ),
-  )
+  ])
 }
