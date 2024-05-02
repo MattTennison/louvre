becfcb9b60e86d9c50f88e46dc0e78557d3b2e62
Matt Tennison <matt@tennison.dev>
2024-05-02 20:04:39 +0100

    fix entrypoint syntax

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { handleRequest } from './handler'
 import { handleScheduled } from './load-data'
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest())
-})
-
-addEventListener('scheduled', (event) => {
-  event.waitUntil(handleScheduled())
-})
+export default {
+  fetch: () => handleRequest(),
+  scheduled: () => handleScheduled(),
+}
